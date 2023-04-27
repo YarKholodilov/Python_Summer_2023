2341908fcb13b3be46a1c3c2ebdbe0bc1016854c
Yaroslav_Kholodilov <131781280+YarKholodilov@users.noreply.github.com>
2023-04-27 22:29:10 +0300

    Update hometask_1_3.py

--- hometask_1_3.py
+++ hometask_1_3.py
@@ -7,8 +7,9 @@ if y == 0:
 
 less_max = -100000 * ((x + y)**2)  # гарантированнно минимальное число
 bigger = (x + y) #  присваиваем максимальной переменной первое возможное значение
+matches = 0
 
-# взял алгорит поиска максимального значения из предыдущего задания
+# взял алгоритм поиска максимального значения из предыдущего задания
 if (x - y) > bigger:
     bigger = (x - y)
 if (x * y) > bigger:
@@ -18,20 +19,33 @@ if (x / y) > bigger:
 if (x // y) > bigger:
     bigger = (x // y)
 
-# ищу второе наибольшее значение после максимального
-if bigger > (x + y) > less_max:
-    less_max = (x + y)
-if bigger > (x - y) > less_max:
-    less_max = (x - y)
-if bigger > (x * y) > less_max:
-    less_max = (x * y)
-if bigger > (x / y) > less_max:
-    less_max = (x / y)
-if bigger > (x // y) > less_max:
-    less_max = (x // y)
+# цикл на поиск совпадений максимумов, если их больше 2, то вывожу максимальное значение
+if bigger == (x + y):
+    matches += 1
+if bigger == (x - y):
+    matches += 1
+if bigger == (x * y):
+    matches += 1
+if bigger == (x / y):
+    matches += 1
+if bigger == (x // y):
+    matches += 1
 
-print(less_max)
-
+if matches >= 2:
+    print(bigger)
+else:     # ищу второе наибольшее значение после максимального
+    if bigger > (x + y) > less_max:
+        less_max = (x + y)
+    if bigger > (x - y) > less_max:
+        less_max = (x - y)
+    if bigger > (x * y) > less_max:
+        less_max = (x * y)
+    if bigger > (x / y) > less_max:
+        less_max = (x / y)
+    if bigger > (x // y) > less_max:
+        less_max = (x // y)
+    print(less_max)
+    
 #проверки:
 # print()
 # print(bigger, less_max)
