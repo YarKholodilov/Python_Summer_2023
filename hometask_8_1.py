62e412afcb6d721f1606a75c6e3ffd4b1acf6e49
coolcooling <yar.kholodilov@gmail.com>
2023-05-19 13:28:29 +0300

    hometask

--- hometask_8_1.py
+++ hometask_8_1.py
@@ -1,21 +1,20 @@
 genomes = input('введите геном на русском языке: ').upper()
-genom = genomes + '*'
 edited = ''
 
+if 'АГ' in genomes:
+    genomes = genomes.replace('АГ', '&&') # ('АГ', 'ГА')
+if 'ГА' in genomes:
+    genomes = genomes.replace("ГА", "@@")
+
+genomes = genomes.replace('@@', 'АГ')
+genomes = genomes.replace('&&', 'ГА')
+genom = genomes + '*'
+
 for i in range(len(genom)-1):
-    if genom[i] == 'А':
-        edited += 'Г'
-    elif genom[i] == 'Г':
-        edited += 'А'
-    elif (genom[i] == 'Ц' and genom[i + 1] == 'Т'):
+    if (genom[i] == 'Ц' and genom[i + 1] == 'Т'):
         edited += genom[i] + 'АГ'
     elif (genom[i] == 'Т' and genom[i + 1] == 'Ц'):
         edited += genom[i] + 'АГ'
     else:
         edited += genom[i]
 print(edited)
-
-
-
-    # if A - G exchange
-    # if C - T or T -C put AG
