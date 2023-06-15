55a2251d001721374ab6695b49b3c1d19cf0d68d
Yaroslav_Kholodilov <131781280+YarKholodilov@users.noreply.github.com>
2023-06-15 13:44:43 +0300

    hometask

--- Hometask_19_2.py
+++ Hometask_19_2.py
@@ -4,12 +4,9 @@ class Fibonacci:
     def __init__(self):
         self.x = 0
         self.y = 1
-        self.limit = math.inf
     def __iter__(self):
         return self
     def __next__(self):
-        if self.x > self.limit:
-            raise StopIteration
         self.x, self.y = self.y, self.x + self.y
         return self.x
 
